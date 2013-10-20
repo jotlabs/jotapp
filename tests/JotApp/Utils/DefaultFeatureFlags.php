1cb387cea469385545f3dbdf6489a80a14a7bd41
Mike Davies <isofarro2@gmail.com>
2013-10-20 19:50:10 +0100

    Simplistic implementation of feature flags

---
+++ tests/JotApp/Utils/DefaultFeatureFlags.php
@@ -0,0 +1,10 @@
+<?php
+
+class DefaultFeatureFlags extends JotApp\Utils\FeatureFlags {
+    protected $flags = array(
+        'positiveFlag' => true,
+        'negativeFlag' => false
+    );
+}
+
+?>
