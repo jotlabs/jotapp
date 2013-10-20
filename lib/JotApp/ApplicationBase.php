47fac52e36a1b0f1bf88b83f72590182a09dafa3
Mike Davies <isofarro2@gmail.com>
2013-10-20 19:51:57 +0100

    A very simple base class for a JotApp-based application, providing hooks and an interface to the core application

---
+++ lib/JotApp/ApplicationBase.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace JotApp;
+
+class ApplicationBase {
+    private static $INSTANCE;
+
+    protected $config;
+    protected $appModel;
+    protected $controllers;
+    protected $dataSources;
+
+    private $controllerCache = array();
+    private $dataSourceCache = array();
+
+    protected function __construct() {
+        //$this->_initAppModel();
+    }
+
+    public static function getInstance() {
+        if (empty(self::$INSTANCE)) {
+            $className = get_called_class();
+            self::$INSTANCE = new $className();
+        }
+
+        return self::$INSTANCE;
+    }
+
+
+    public function getAppModel() {
+        $appModel = $this->_initAppModel();
+        return $appModel;
+    }
+
+
+    public function getController($controllerName) {
+        if (empty($this->controllerCache[$controllerName])) {
+            $className = $this->controllers[$controllerName];
+
+            if (class_exists($className)) {
+                $appModel = $this->getAppModel();
+                $controllerClass = new $className($appModel);
+
+                if (is_a($controllerClass, 'JotApp\Controller')) {
+                    $this->controllerCache[$controllerName] = $controllerClass;
+                }
+
+            }
+        }
+
+        return $this->controllerCache[$controllerName];
+    }
+
+
+    public function setConfig($config) {
+        $this->config = $config;
+    }
+
+    public function getDomain() {
+        return $this->config->domain;
+    }
+
+    public function getFacebookAppId() {
+        return $this->config->facebookAppId;
+    }
+
+
+
+    protected function _initAppModel() {
+        if (empty($this->appModel) && $this->appModelClass && class_exists($this->appModelClass)) {
+            $className = $this->appModelClass;
+            $appModel = new $className();
+            $appModel->addDataSource($this->config->datasources['__DEFAULT__']);
+            $this->appModel = $appModel;
+        }
+
+        return $this->appModel;
+    }
+
+
+}
+
+?>
